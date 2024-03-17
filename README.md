9716c8d0bb593ca135b8e7e541c3d53016ba4951
zMynx <lior.dux@develeap.com>
2024-03-17 20:35:24 +0200

    prefer docker

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # java
 Jave repo for my Open University course
+
+# Syntax
+- Naming convention: camelCase.
+- Class naming: PascalCase.
+- Attributes naming: prefied with underscore `_`.
+- constanst naming: UPPERCASE.
--- app/hello_world.java
+++ app/hello_world.java
@@ -1,12 +1,24 @@
-// This is a simple Java program.
-// FileName : "HelloWorld.java".
-
-
-class HelloWorld {
-	// Your program begins with a call to main().
-	// Prints "Hello, World" to the terminal window.
-	public static void main(String args[])
-	{
+/**
+ * @file: hello_world.java
+ * @autor: 1155050023
+ * @date: 2016-08-31
+ * @version: 1.0
+ * @description:
+ * 
+ */
+import java.util.Scanner;
+public class HelloWorld {
+	/**
+	 * @description: Prints "Hello, World" to the terminal window.
+	 * @param args
+	 * @return void
+	 */
+	public static void main(String args[]){
 		System.out.println("Hello, World");
+    Scanner buffer = new Scanner(System.in);
+    String name;
+    System.out.println("What is your name?");
+    name = buffer.nextLine();
+    System.out.println("Hello, " + name);
 	}
 }
